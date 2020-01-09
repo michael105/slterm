20aadad1fe025af6b0b8871d23bbb3d4e58c4d56
Michael Myer <misc.myer@zoho.com>
2020-01-09 21:42:10 +0100

    SWAP via xor

--- config.def.h
+++ config.def.h
@@ -242,7 +242,7 @@ static Shortcut shortcuts[] = {
     { TERMMOD, XK_Num_Lock, numlock, {.i = 0} },
     { ShiftMask, XK_Page_Up, kscrollup, {.i = -1} },
     { ShiftMask, XK_Page_Down, kscrolldown, {.i = -1} },
-    { TERMMOD, XK_Escape, keyboard_select, { 0 } },
+    { TERMMOD, XK_S, keyboard_select, { 0 } },
 };
 
 /*
