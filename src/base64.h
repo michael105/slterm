a35dfdfdd93bcfe01374c5ef38d5b3ecba9c232e
Michael Myer <misc.myer@zoho.com>
2020-01-16 15:23:59 +0100

    splitting sources

---
+++ src/base64.h
@@ -0,0 +1,10 @@
+#ifndef base64_h
+#define base64_h
+
+
+char *base64dec(const char *src);
+
+
+
+
+#endif
---
+++ src/stsrc.c
@@ -0,0 +1,30 @@
+// Including all sources into a single files
+// leaves more possibilities for optimization to the compiler.
+// 
+
+
+#include "includes.h"
+
+#include "st.h"
+#include "x.h"
+#include "system.h"
+#include "tty.h"
+#include "selection.h"
+
+#define DEBUG_FILELEVEL 1
+#include "debug.h"
+
+#include "st.c"
+#include "mem.c"
+#include "base64.c"
+#include "selection.c"
+#include "x.c"
+#include "main.c"
+#include "system.c"
+#include "tty.c"
+#include "utf8.c"
+
+
+
+
+
