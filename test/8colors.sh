603f2bcf71adae641a7d01c9841851659798cee5
misc147 <misc7@posteo.net>
2024-10-01 01:54:41 +0000

    change color palette

--- test/8colors.sh
+++ test/8colors.sh
@@ -7,20 +7,20 @@ echo
 for a in 2 0 1 '1;2'; do
 	printf '\033[37;0m%3sm: ' $a
 	for b in `seq 30 37`;do 
-		printf "\033[$a;$b"'m %03d ' $b
+		printf "\033[$a;$b"'m  %03d  ' $b
 	done
 	printf "\033[0m\n"
 done
 
 echo
 
-echo -e "\n\033[1;37mInverse\e[0m";
+echo -e "\n\033[1;37mInverse\e[0m"
 echo
 
 for a in 2 0 1 '1;2'; do
 	printf '\033[37;0m%3sm: ' $a
 	for b in `seq 30 37`;do 
-		printf "\033[$a;7;$b"'m %03d ' $b
+		printf "\033[$a;7;$b"'m  %03d  ' $b
 	done
 	printf "\033[0m\n"
 done
