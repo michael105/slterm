be6472ee0bf18f97475533025a3b700bf5d536e6
misc147 <misc7@posteo.net>
2024-10-12 15:13:00 +0000

    renaming

--- src/config.h
+++ src/config.h
@@ -92,7 +92,7 @@ char *termname = "st-256color";
  * spaces per tab
  *
  * When you are changing this value, don't forget to adapt the »it« value in
- * the st.info and appropriately install the st.info in the environment where
+ * the slterm.info and appropriately install the slterm.info in the environment where
  * you use this st version.
  *
  *	it#$tabspaces,
@@ -506,7 +506,7 @@ BIND( ControlMask|Mod4Mask, XK_0, set_charmap, { .i=0 },ALLMODES ),
 #undef BIND
 
 /*
- * Special keys (change & recompile st.info accordingly)
+ * Special keys (change & recompile slterm.info accordingly)
  *
  * Mask value:
  * * Use XK_ANY_MOD to match the key no matter modifiers state
