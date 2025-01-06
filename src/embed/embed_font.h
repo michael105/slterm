31e619b9854ac3054fb2250f86c82a4f08769435
misc147 <misc7@posteo.net>
2025-01-06 14:32:06 +0000

    Embedded empty fonts

--- src/embed/embed_font.h
+++ src/embed/embed_font.h
@@ -7,11 +7,11 @@ extern unsigned char slterm_font_ttf[];
 extern unsigned int slterm_font_italic_ttf_len;
 extern unsigned char slterm_font_italic_ttf[];
 
-extern unsigned int slterm_font_bold_ttf_len = 0;
-extern unsigned char slterm_font_bold_ttf[] = 0;
+extern unsigned int slterm_font_bold_ttf_len;
+extern unsigned char slterm_font_bold_ttf[];
 
-extern unsigned int slterm_font_bold_italic_ttf_len = 0;
-extern unsigned char slterm_font_bold_italic_ttf[] = 0;
+extern unsigned int slterm_font_bold_italic_ttf_len;
+extern unsigned char slterm_font_bold_italic_ttf[];
 
 
 
