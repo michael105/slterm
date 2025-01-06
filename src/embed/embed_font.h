f2e480b540eb2122f472726c561142ba8fa4833b
misc147 <misc7@posteo.net>
2025-01-06 14:31:47 +0000

    Embedded empty fonts

--- src/embed/embed_font.h
+++ src/embed/embed_font.h
@@ -1,11 +1,18 @@
 #pragma once
 
 
-extern unsigned int slterm_font_italic_ttf_len;
 extern unsigned int slterm_font_ttf_len;
-
-
-extern unsigned char slterm_font_italic_ttf[];
 extern unsigned char slterm_font_ttf[];
+
+extern unsigned int slterm_font_italic_ttf_len;
+extern unsigned char slterm_font_italic_ttf[];
+
+extern unsigned int slterm_font_bold_ttf_len = 0;
+extern unsigned char slterm_font_bold_ttf[] = 0;
+
+extern unsigned int slterm_font_bold_italic_ttf_len = 0;
+extern unsigned char slterm_font_bold_italic_ttf[] = 0;
+
+
 
 
