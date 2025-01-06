cfa8ac3503e35a32e8d7c276f570e3658f3787ea
misc147 <misc7@posteo.net>
2025-01-06 02:14:51 +0000

    Embedded fonts

---
+++ src/embed/embed_font.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+extern unsigned int slterm_font_italic_ttf_len;
+extern unsigned int slterm_font_ttf_len;
+
+
+extern unsigned char slterm_font_italic_ttf[];
+extern unsigned char slterm_font_ttf[];
+
+
